dd3b751c4a364825d531c55568ea27b273aeebc4
Nikita <nrw@narola.email>
2023-08-22 11:37:30 +0530

    Change

--- src/main/java/com/example/demomvc/AddController.java
+++ src/main/java/com/example/demomvc/AddController.java
@@ -1,24 +1,24 @@
-//package com.example.demomvc;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//@Controller
-//public class AddController {
-//
-//
-//    @RequestMapping("add")
-//    public  ModelAndView add(@RequestParam("t1") int num1,@RequestParam("t2") int num2)
-//    {
-//
-//        ModelAndView modelAndView=new ModelAndView();
-//        int k =num1+num2;
-//        modelAndView.addObject("result",k);
-//        modelAndView.setViewName("result.jsp");
-//
-//        return modelAndView;
-//    }
-//
-//}
+package com.example.demomvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddController {
+
+
+    @RequestMapping("add")
+    public  ModelAndView add(@RequestParam("t1") int num1,@RequestParam("t2") int num2)
+    {
+
+        ModelAndView modelAndView=new ModelAndView();
+        int k =num1+num2;
+        modelAndView.addObject("result",k);
+        modelAndView.setViewName("result.jsp");
+
+        return modelAndView;
+    }
+
+}
--- src/main/java/com/example/demomvc/Config.java
+++ src/main/java/com/example/demomvc/Config.java
@@ -1,13 +1,13 @@
-//package com.example.demomvc;
-//
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//
-//@Configuration
-//@ComponentScan("com.example.demomvc")
-//@EnableWebMvc
-//public class Config extends WebMvcConfigurerAdapter {
-//}
+package com.example.demomvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+@ComponentScan("com.example.demomvc")
+@EnableWebMvc
+public class Config extends WebMvcConfigurerAdapter {
+}
