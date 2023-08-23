d366cecf33deb72786e6c8f5a6afac19b1705203
Nikita <nrw@narola.email>
2023-08-23 14:21:36 +0530

    Change

--- src/main/java/com/example/demomvc/MyFrontController.java
+++ src/main/java/com/example/demomvc/MyFrontController.java
@@ -1,9 +1,6 @@
 package com.example.demomvc;
 
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 
